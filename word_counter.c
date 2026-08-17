8a01305b3500f427996a35df4005ecbbe5e1af9a
ahyoungshin12 <say20060120@gmail.com>
2026-08-17 17:08:06 +0900

    Create word_counter.c
    A C program that reads a text file containing President Obama's speech (Obama_Speech.txt) and calculates the total occurrences of a user-specified word.

---
+++ word_counter.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int num = 0;
+    FILE *fp = fopen("data/Obama_Speech.txt", "r");
+    printf("Input the word you want to extract : ");
+
+    // Write your code here
+    char input[20];
+    char word[100];
+    int i, len;
+
+    if (scanf("%19s", input) == 1) {
+        while (fscanf(fp, "%99s", word) != EOF) {
+            // 단어 뒤에 붙은 문장부호(. , ! ? " ') 제거
+            len = strlen(word);
+            while (len > 0) {
+                char ch = word[len - 1];
+                if (ch == '.' || ch == ',' || ch == '!' || ch == '?' || ch == '"' || ch == '\'') {
+                    word[len - 1] = '\0';
+                    len--;
+                } else {
+                    break;
+                }
+            }
+
+            // 일치하는 단어 카운트
+            if (strcmp(input, word) == 0) {
+                num++;
+            }
+        }
+    }
+
+    printf("Number of input words in Obama's speech : %d", num);
+    if (fp != NULL) fclose(fp);
+    return 0;
+}
